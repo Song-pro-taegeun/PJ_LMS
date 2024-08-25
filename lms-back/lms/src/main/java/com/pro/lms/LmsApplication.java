90bd517d0810a903e4b4b86e84fba365b9ff75fa
Song-pro-taegeun <xormsdlrnt@naver.com>
2024-08-25 17:35:50 +0900

    추가 셋팅 (임시)

--- lms-back/lms/src/main/java/com/pro/lms/LmsApplication.java
+++ lms-back/lms/src/main/java/com/pro/lms/LmsApplication.java
@@ -2,12 +2,18 @@ package com.pro.lms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableCaching
+@EnableJpaAuditing
 @SpringBootApplication
 public class LmsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(LmsApplication.class, args);
+		System.out.println("------------------------------------------------------------------------------------------------");
+        System.out.println("START LMS APPLICATION");
+		System.out.println(args);
+        System.out.println("------------------------------------------------------------------------------------------------");
 	}
-
 }
