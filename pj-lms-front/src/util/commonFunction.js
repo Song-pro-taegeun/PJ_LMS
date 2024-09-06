9b9a9572c388b4223a1e1232555b40f6ea386051
khb4130 <khb4130@naver.com>
2024-09-06 20:40:09 +0900

    Password Reset 로직 작업 완료

--- pj-lms-front/src/util/commonFunction.js
+++ pj-lms-front/src/util/commonFunction.js
@@ -3,3 +3,10 @@ export const validateEmail = (email) => {
   const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
   return emailPattern.test(email);
 };
+
+// 비밀번호 정규식 체크 공통함수
+export const validatePassword = (password) => {
+  const passwordPattern =
+    /^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!@#$%^&*()-]).{8,20}$/;
+  return passwordPattern.test(password);
+};
