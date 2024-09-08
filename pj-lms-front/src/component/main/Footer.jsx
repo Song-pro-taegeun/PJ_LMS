c75c43d4858547abbc3ca1b10c667850270a8076
SoohyunLee <apdlfdlwkd@naver.com>
2024-09-08 16:45:39 +0900

    footer 퍼블리싱 완료

--- pj-lms-front/src/component/main/Footer.jsx
+++ pj-lms-front/src/component/main/Footer.jsx
@@ -1,7 +1,23 @@
 export default function Footer() {
   return (
     <>
-      <div className="footer">footer</div>
+      <div className="footer">
+        <div className="wrap">
+          <div className="logo">Code Lap</div>
+          <div className="info">
+            <span>코드랩</span>
+            <ul>
+              <li>주소. 1234 상상로, 7층 환상시, 가상구 56789 상상의 나라</li>
+              <ul>
+                <li>Tel. 02-1234-5678</li>
+                <li>Fax. 02-1234-5678</li>
+                <li>Email. 홍길동@codelap.com</li>
+              </ul>
+            </ul>
+            <p>© Code Lap. All rights reserved.</p>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
